bc5a72649ebcd96226a1dbd469851badd324a4b5
Mohammad-Usman1 <mohammadusman1324@gmail.com>
2024-08-27 01:12:36 -0700

    Finalized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Components/Navbar"
+import Main from "./Components/Main"
+
+class App extends React.Component {
+
+  render() {
+    return <>
+      <Navbar />
+      <Main />
+    </>
+  }
+}
+export default App;
+// Template Link
+// https://websitedemos.net/plant-store-02/
